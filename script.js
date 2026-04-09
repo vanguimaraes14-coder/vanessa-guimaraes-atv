fb1bcd64d1dd6841e52d37fc4ab1a48e32ec70a5
vanguimaraes14-coder <van.guimaraes14@gmail.com>
2026-04-09 13:19:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,117 +1,22 @@
-const texto = [
-"Desenvolvedora Front-End",
-"Estudante de Sistemas para Internet"
-];
-
-let contador = 0;
-let letra = 0;
-
-function digitar(){
-
-const elemento = document.querySelector(".digitando");
-
-if(letra < texto[contador].length){
-
-elemento.innerHTML += texto[contador].charAt(letra);
-
-letra++;
-
-setTimeout(digitar,80);
-
-}else{
-
-setTimeout(apagar,2000);
-
-}
-
-}
-
-function apagar(){
-
-const elemento = document.querySelector(".digitando");
-
-if(letra > 0){
-
-elemento.innerHTML = texto[contador].substring(0,letra-1);
-
-letra--;
-
-setTimeout(apagar,50);
-
-}else{
-
-contador++;
-
-if(contador >= texto.length){
-
-contador = 0;
-
-}
-
-setTimeout(digitar,500);
-
-}
-
-}
-
-digitar();
-
-
 const projetos = [
 
+
 {
-
-titulo:"Portfólio Pessoal",
-
-descricao:"Portfólio desenvolvido para apresentar meus projetos e habilidades em desenvolvimento web.",
-
-link:"#",
-
-tecnologias:["HTML","CSS","JavaScript"]
-
+titulo: "Portfólio Profissional",
+descricao: "Site pessoal desenvolvido com HTML, CSS e JavaScript.",
+link: "#"
 },
 
 {
+titulo: "Lista de Tarefas em JavaScript",
+descricao: "Aplicação simples para organizar tarefas utilizando JavaScript.",
+link: "#"
+},
 
-titulo:"Sistema de Gestão",
-
-descricao:"Aplicação web para gerenciamento de projetos com interface responsiva.",
-
-link:"#",
-
-tecnologias:["HTML","CSS","JavaScript"]
-
+{
+titulo: "Calculadora Web",
+descricao: "Calculadora criada com HTML, CSS e JavaScript.",
+link: "#"
 }
 
 ];
-
-
-function carregarProjetos(){
-
-const container=document.getElementById("projetos-container");
-
-projetos.forEach(projeto=>{
-
-const card=document.createElement("div");
-
-card.classList.add("card");
-
-card.innerHTML=`
-
-<h3>${projeto.titulo}</h3>
-
-<p>${projeto.descricao}</p>
-
-<p><strong>Tecnologias:</strong> ${projeto.tecnologias.join(", ")}</p>
-
-<a href="${projeto.link}" target="_blank">Ver Projeto</a>
-
-`;
-
-container.appendChild(card);
-
-});
-
-}
-
-carregarProjetos();
